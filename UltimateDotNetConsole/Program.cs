f17e102f1afefaa8213b18d84c6cf61d5910968c
Robert Lyn <lenny4422@gmail.com>
2022-10-20 17:26:29 -0400

    Hookup DI and write message

--- UltimateDotNetConsole/Program.cs
+++ UltimateDotNetConsole/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace UltimateDotNetConsole;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Library.BusinessLogic;
+
+namespace UltimateDotNetConsole;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using IHost host = CreateHostBuilder(args).Build();
+        using var scope = host.Services.CreateScope();
+
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            services.GetRequiredService<App>().Run(args);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) =>
+            {
+                services.AddSingleton<IMessages, Messages>();
+                services.AddSingleton<App>();
+            });
+
+        throw new Exception();
     }
 }
