4de8108c2f8856e17beed0cadf1abadbcef22291
Robert Lyn <lenny4422@gmail.com>
2022-10-22 02:28:12 -0400

    Improve Messages to set lanuage variable

--- UltimateDotNetConsole/App.cs
+++ UltimateDotNetConsole/App.cs
@@ -18,19 +18,7 @@ namespace UltimateDotNetConsole
 
         public void Run(string[] args)
         {
-            string lang = "en";
-
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i].ToLower().StartsWith("-lang="))
-                {
-                    lang = args[i].Substring(6);
-                    break;
-                }
-            }
-
-            string message = _messages.Greeting(lang);
-            Console.WriteLine(message);
+            Console.WriteLine(_messages.Greeting);
         }
     }
 }
